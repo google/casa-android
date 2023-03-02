e7a3ae72b70fbbaacfaccc338531d6158326ed86
Marcel Pintó Biescas <marcelpinto@google.com>
2023-03-02 13:45:48 +0100

    Add large screen support
    Create an adaptive dual-pane that will show a list-detail screen with the list of samples and the selected sample.
    
    Currently only Composable samples support that, for the rest it will expand to full screen.
    
    This allows better navigation and creates a great experience for large screens.
    
    In addition launch URL into multi-window mode when in large screen to allow side-by-side view with the code and the selected sample.

--- app-catalog/samples/compose/src/main/java/com/google.android.catalog.app.compose/ComposeSample.kt
+++ app-catalog/samples/compose/src/main/java/com/google.android.catalog.app.compose/ComposeSample.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.google.android.catalog.framework.annotations.Sample
 
@@ -30,7 +31,7 @@ import com.google.android.catalog.framework.annotations.Sample
 )
 @Composable
 fun ComposeSample() {
-    Box(Modifier.fillMaxSize()) {
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text(text = "Hi, I am a compose sample target!")
     }
 }
--- app-catalog/samples/multiple/src/main/java/com/google.android.catalog.app.multiple/DeprecatedSample.kt
+++ app-catalog/samples/multiple/src/main/java/com/google.android.catalog.app.multiple/DeprecatedSample.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.google.android.catalog.framework.annotations.Sample
 
@@ -30,7 +31,7 @@ import com.google.android.catalog.framework.annotations.Sample
 @Sample(name = "Deprecated sample", "Shows how a sample can be deprecated")
 @Composable
 fun DeprecatedSample() {
-    Box(Modifier.fillMaxSize()) {
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text(text = "Hi, you won't see me!")
     }
 }
